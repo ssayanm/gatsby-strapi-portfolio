828b15d8a8f8ef048427223aef0b2d09c780a48c
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-05-29 15:42:10 +0530

    added the service section and added backende strapi api

---
+++ backend-api/config/database.js
@@ -0,0 +1,32 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        client: "mongo",
+        uri: env("DATABASE_URI"),
+        database: env("DATABASE_NAME"),
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+})
---
+++ backend-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '208ab158-764d-4c94-aba0-ebf6296cbff8'
+};
