21c4780a66aac7dfecea48b4a0c5dc20beb5b83f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-06-02 11:55:04 +0530

    added prop types

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,7 +8,7 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
       <Image fluid={image.childImageSharp.fluid} className="project-img" />
       <div className="project-info">
         <span className="project-number">0{index + 1}.</span>
-        <h3>{title}</h3>
+        <h3>{title || "default title"}</h3>
         <p className="project-desc">{description}</p>
         <div className="project-stack">
           {stack.map(item => {
@@ -28,6 +28,12 @@ const Project = ({ description, title, github, stack, url, image, index }) => {
   )
 }
 
-Project.propTypes = {}
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  stack: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
 
 export default Project
