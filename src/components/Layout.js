daa82611cfb9cc1812bcbdbc5a41a864104f92ce
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-06-02 16:49:12 +0530

    added the sidebar for the menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 // import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleSidebar = () => {
+    return setIsOpen(!isOpen)
+  }
   return (
     <React.Fragment>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </React.Fragment>
